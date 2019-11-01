55e689345a4ace90cf2848caa3a1e8946be4cd6f
Kritsatorn <kritsatorn@billme.co.th>
2019-11-02 01:01:29 +0700

    Updates

---
+++ precache-manifest.2dbbc8c4f0267c8c51d6f3b82cb519a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc19c51a24b6cb49477cc7c2c819696",
+    "url": "/Kritsatorn/YWC_HW_TS/web/index.html"
+  },
+  {
+    "revision": "93fe93939f27a4753f72",
+    "url": "/Kritsatorn/YWC_HW_TS/web/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "5137b63de423cec64eec",
+    "url": "/Kritsatorn/YWC_HW_TS/web/static/css/main.48c56240.chunk.css"
+  },
+  {
+    "revision": "93fe93939f27a4753f72",
+    "url": "/Kritsatorn/YWC_HW_TS/web/static/js/2.84e1462d.chunk.js"
+  },
+  {
+    "revision": "5137b63de423cec64eec",
+    "url": "/Kritsatorn/YWC_HW_TS/web/static/js/main.98ed98e3.chunk.js"
+  },
+  {
+    "revision": "ccd716ac2b34a16581f6",
+    "url": "/Kritsatorn/YWC_HW_TS/web/static/js/runtime-main.99aff235.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Kritsatorn/YWC_HW_TS/precache-manifest.11e1389b340431634706f7d94462a981.js"
+  "/Kritsatorn/YWC_HW_TS/web/precache-manifest.2dbbc8c4f0267c8c51d6f3b82cb519a0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Kritsatorn/YWC_HW_TS/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Kritsatorn/YWC_HW_TS/web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
